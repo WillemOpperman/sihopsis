b2a85c1361e6a0c89c0428d6d3e07d27062ab9c1
Willem Opperman <willem.n.opperman@gmail.com>
2015-02-28 18:18:43 +0200

    rails_apps_composer: initial commit

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -1,7 +1,7 @@
 <% module_namespacing do -%>
 class <%= class_name %> < <%= parent_class_name.classify %>
 
-  has_paper_trail
+  #has_paper_trail
 
 <% attributes.select {|attr| attr.reference? }.each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
