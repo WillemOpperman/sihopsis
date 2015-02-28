efdb10db4f3341e6a351c2eba04e287de919a6d3
Willem Opperman <willem.n.opperman@gmail.com>
2015-02-28 17:51:53 +0200

    rails_apps_composer: devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable, :confirmable, :confirmable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable, :confirmable, :confirmable, :confirmable, :confirmable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
